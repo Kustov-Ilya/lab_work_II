f5a132904d7251953028ff9c7e3c23e126096dbc
Kustov-Ilya <kustoff.il@yandex.ru>
2019-03-26 18:51:24 +0300

    Fixed lab3

--- lab3.py
+++ lab3.py
@@ -3,14 +3,12 @@ import itertools
 import matplotlib.pyplot as plt 
 import math
 
-
 def RBF():
     number_of_1 = [Function(i) for i in itertools.product([0, 1], repeat = 4)].count(1)
     if number_of_1 <=8:
         return [i for i in itertools.product([0, 1], repeat = 4) if Function(i)]
     else:
         return [i for i in itertools.product([0, 1], repeat = 4) if not Function(i)]
-
 
 def Train(selection):
     nu = 0.3
@@ -45,7 +43,6 @@ def Train(selection):
         print(f"\nTest don't accessed\nNeuron can't learn\n{'*' * 70}")
         return 50
 
-
 def Drow_plot(era, list_errors):
     X = [i for i in range(era)]
     Y = [list_errors[i] for i in X]
@@ -53,7 +50,6 @@ def Drow_plot(era, list_errors):
     plt.xlabel('Eras')
     plt.ylabel('Errors')
     plt.show()
-
 
 #будевый вектор, который высчитал нейрон
 def Neuron_vector(c, u):
@@ -65,7 +61,6 @@ def Neuron_vector(c, u):
         out += str(y)
     return out
 
-
 #реальный будевый вектор
 def Real_vector():
     real = ''
@@ -73,16 +68,13 @@ def Real_vector():
         real+=str(Function(i))
     return real
 
-
 #значения в RBF-нейронах
 def Compute_fi(c, x):
     difference= [[np.array(x) - np.array(ci)] for ci in c]
     return [1] + [math.exp(-np.sum(np.power(diff, 2))) for diff in difference]
 
-
 def Function(x):
     return int(not (x[0] or x[1]) or x[2] or x[3])
-
 
 def Selection():
     Set = [list(i) for i in itertools.product([0, 1], repeat=4)]
@@ -90,7 +82,6 @@ def Selection():
             for rezult_selection in itertools.permutations(Set, i):
                 if Train(list(rezult_selection)) < 50:
                     return None
-
 
 if __name__ == "__main__":
     selection =  [[0, 0, 0, 1], [1, 1, 0, 0]]
