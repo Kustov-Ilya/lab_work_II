4c35396a63d559e31a06cea737e16b12c3337680
Kustov-Ilya <kustoff.il@yandex.ru>
2019-03-26 18:45:07 +0300

    Fixed

--- lab2.py
+++ lab2.py
@@ -20,26 +20,31 @@ def Train(p, eras, nu, isplot = True):
     print(f'Era:{eras} nu = {nu} p = {p}\nVector w:{np.around(w, 3)}')
     return Plot(w, p) if isplot else (w)
 
-def Plot(w, p):
-    x = np.linspace(-4, 8, 30)
-    matrix = np.array([[Function(x[i + j]) for j in range(p)] for i in range(30 - p)])
+def Real_Plot():
+    x = np.linspace(-4,4, 20)
     plt.plot(x, [Function(t) for t in x])
-    plt.plot(x[p:], [np.dot(w, arr) for arr in matrix])
+
+
+def Plot(w, p):
+    x = np.linspace(-4, 12, 40)
+    matrix = np.array([[Function(x[i + j]) for j in range(p)] for i in range(20-p, 40 - p)])
+    plt.plot(x, [Function(t) for t in x])
+    plt.plot(x[20:], [np.dot(w, arr) for arr in matrix])
     print(f'Error: {np.around(Get_error(w, p), 4)}')
     plt.show()
 
 def Get_error(w, p):
-    x = np.linspace(-4, 8, 30)
-    matrix = np.array([[Function(x[i + j]) for j in range(p)] for i in range(30 - p)])
+    x = np.linspace(-4, 8, 40)
+    matrix = np.array([[Function(x[i + j]) for j in range(p)] for i in range(20-p, 40 - p)])
     Real_line = np.array([Function(t) for t in x])
     Neuron_line = [np.dot(w, arr) for arr in matrix]
-    error = Real_line[p:]-Neuron_line
+    error = Real_line[20:]-Neuron_line
     return sum([a**2 for a in error])
 
 def Function(t):
     return 0.4 * math.sin(0.3 * t) + 0.5
 
-def test_nu():
+def Test_nu():
     error_list = []
     for i in np.arange(0.1, 1.1, 0.1):
         w = Train(6, 2000, i, False)
@@ -49,7 +54,7 @@ def test_nu():
     plt.ylabel("Errors")
     plt.show()
 
-def test_p():
+def Test_p():
     error_list = []
     for i in np.arange(1, 17):
         w = Train(i, 500, 0.4, False)
@@ -59,7 +64,7 @@ def test_p():
     plt.ylabel("Errors")
     plt.show()
 
-def test_era():
+def Test_era():
     error_list = []
     for i in np.arange(1000, 5000, 1000):
         w = Train(6, i, 0.4, False)
@@ -72,11 +77,9 @@ def test_era():
         
 
 if __name__ == "__main__":
-    Train(5, 2000, 0.4)
-    test_nu()
-    test_p()
-    test_era()
+    Train(6, 2000, 1)
+    #Real_Plot()
+    #Test_nu()
+    #Test_p()
+    #Test_era()
 
-    #Lets_test(0, 2000)
-    #   окно, эпох, график                             a+bx
-    #окно 2 = 0.0069(50к) 3 = 0.0056(100к) 4 = 0.0013 5 = 9e-5 6 =  2e-6
